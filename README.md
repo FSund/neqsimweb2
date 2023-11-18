db06a62c7d60d9f47e3a05097e17cf1a200b4443
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-11-18 12:27:21 +0000

    first test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# neqsimweb2
+# Welcome to NeqSimWeb2
+NeqSimWeb2 is a web user interface for the [neqsim process simulator](https://equinor.github.io/neqsimhome/)
---
+++ neqsim_app.py
@@ -0,0 +1,24 @@
+import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.image('images/neqsimlogocircleflat.png', width=150)
+
+"""
+# Welcome to NeqSim!
+
+NeqSim is a library for calculation of fluid behavior, phase equilibrium and process simulation. This this project is a collectiomn of Streamlit models using neqsim for simulation.The method documentation for the models found in this application can be found in [Introduction to Gas Processing using NeqSim in Colab](https://colab.research.google.com/github/EvenSol/NeqSim-Colab/blob/master/notebooks/examples_of_NeqSim_in_Colab.ipynb).
+
+## NeqSim project in GitHub
+The NeqSim library is written in the Java programming language. The source code and libraries are hosted in GitHub.
+
+* [NeqSim Java](https://github.com/equinor/neqsim)
+
+## NeqSim discussion
+Questions related to use and development are asked on the [NeqSim github discussions](https://github.com/equinor/neqsim/discussions) page.
+
+## How to use this application
+Use right menu to select operations
+
+"""
