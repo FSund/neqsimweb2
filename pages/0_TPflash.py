0dd2785a3d9dfc1351b8be14e134ddbeb5cdc28a
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-12-16 19:35:52 +0000

    update

--- pages/0_TPflash.py
+++ pages/0_TPflash.py
@@ -54,6 +54,6 @@ if st.button('Run'):
     result_string = delimiter.join(string_list)
     delimiter = ", "
     result_string = delimiter.join(string_list)
-    input = "what scientific experimental equilibrium data are available for mixtures of s" + result_string + " at temperature " + str(temp) + " Celcius and " + str(pressure) + " bara"  
+    input = "what scientific experimental equilibrium data are available for mixtures of s" + result_string + " at temperature around " + str(temp) + " Celcius and pressure around " + str(pressure) + " bara"  
     openapitext = st.question(input)
     st.write(openapitext)
