d882162083b1a380d4b1f4f439ba3f935a9d1fa5
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-12-17 00:38:51 +0000

    update

--- pages/40_PTenvelope.py
+++ pages/40_PTenvelope.py
@@ -4,19 +4,14 @@ import neqsim
 from neqsim.thermo import fluid_df, phaseenvelope, TPflash, dataFrame
 from neqsim import jNeqSim
 import matplotlib.pyplot as plt
+from fluids import detailedHC_data
 
 st.title('Phase Envelope')
 
 st.text("Set fluid composition:")
 # Sample data for the DataFrame
-default_data = {
-    'ComponentName':  ["nitrogen", "CO2", "methane", "ethane", "propane", "i-butane", "n-butane", "i-pentane", "n-pentane", "n-hexane", "benzene", "c-hexane", "n-heptane", "c-C7", "toluene","n-octane", "m-Xylene","c-C8","n-nonane","nC10","nC11","nC12", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20"],
-    'MolarComposition[-]':  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-    'MolarMass[kg/mol]': [None, None, None, None, None, None, None, None, None, None, None, None, None,None, None, None, None,None, None, None,None, None, 0.0913, 0.1041, 0.1188, 0.136, 0.150, 0.164, 0.179, 0.188, 0.204, 0.216, 0.236, 0.253, 0.27, 0.391],
-    'RelativeDensity[-]': [None, None, None, None, None, None, None, None, None, None, None, None, None,None, None, None, None,None, None, None,None, None, 0.746, 0.768, 0.79, 0.787, 0.793, 0.804, 0.817, 0.83, 0.835, 0.843, 0.837, 0.84, 0.85, 0.877]
-}
 
-df = pd.DataFrame(default_data)
+df = pd.DataFrame(detailedHC_data)
 st.edited_df = st.data_editor(
     df,
     column_config={
